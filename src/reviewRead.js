5596b4a91e6b3ddafbbf65d1881189f73b129ea7
HaeRheeNam <4ran0318@gmail.com>
2024-01-11 14:29:56 +0900

    [Feat/#7] review 조회 기능 -getItem으로 자료 받기 전 완성

--- src/reviewRead.js
+++ src/reviewRead.js
@@ -1,46 +1,45 @@
 //reviewRead.js - 해당 영화의 상세페이지의 댓글 내용을
-// 예시 데이터
-const $section = document.querySelector("#review");
-console.log($section);
-const review = {
+export const $section = document.querySelector("#reviewRead");
+export const $movieId =
+  document.querySelector(".movie-id").attributes["movieid"].value;
+export const $readId = document.querySelectorAll("#reviewRead");
+const reviews = {
   data: [
     {
-      id: "1",
+      movieid: `${$movieId}`,
+      reviewid: "2",
       name: "멍멍",
       review: "최고의 영화였습니다.",
       password: 1234,
     },
     {
-      id: "2",
+      movieid: `${$movieId}`,
+      reviewid: "3",
       name: "야옹",
       review: "완전 최고의 영화였습니다.",
       password: 5678,
     },
   ],
 };
-const movieidArr = [
-  { movieid: "278", title: "쇼생크머시기" },
-  { movieid: "238", title: "대부쨩" },
-];
-//localstorage.setItem("review" : )
-console.log({ review: review.data });
 
-const datas = [...review.data, ...movieidArr];
+//localstorage.setItem("review" : )
+console.log({ review: reviews.data });
+
+const datas = [...reviews.data];
 console.log(datas);
 
-review.data.map((arr) => {
-  const { id, name, review, password } = arr;
+reviews.data.map((arr) => {
+  const { reviewid, name, review, password, movieid } = arr;
   return $section.insertAdjacentHTML(
     "beforeend",
     `
-          <div readid="${id}">
-              <p>${review}</p>
-              <p>작성자: ${name}</p>
-              <input type="password" placeholder="password" />
-              <button type="submit" id="">삭제</button>
+           <div movidid="${movieid}">
+               <div readid="${reviewid}">id: ${reviewid}</div>
+               <p>${review}</p>
+               <p>작성자: ${name}</p>
+               <input type="password" placeholder="password" />
+               <button type="submit" id="">삭제</button>
             </div>
         `
   );
 });
-
-// localStorage.setItem("review", JSON.stringify(data));
