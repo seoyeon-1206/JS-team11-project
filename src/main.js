dc8752e5aeff729b4253f04850320c8fa09b7967
KY <rudyeon@naver.com>
2024-01-11 10:32:47 +0900

    [Feat/#9] search.js 파일 생성 및 main.js 수정

--- src/main.js
+++ src/main.js
@@ -1,82 +1,15 @@
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOTM3YTE2N2YxZTA5ZGYwNDM4ZDI1NmZiNThjZTQ0YyIsInN1YiI6IjY1OTk4NGI3MWQxYmY0MDFhOGU1NWZiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TAtSAqF7Eikmbg_lzzouG1BSYyyVwe8MO9Hkr-0XTwg",
-  },
-};
+// 1. 영화 데이터를 가져와서 화면에 출력
+import { showMovieList } from "./movie.js";
+showMovieList();
 
-fetch(
-  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
-  options
-)
-  .then((response) => response.json())
-  .then((response) => {
-    let movieList = response["results"];
+// 2. 영화 검색
+import { filterArr } from "./search.js";
 
-    movieList.forEach((element) => {
-      let title = element["title"];
-      let overview = element["overview"];
-      let poster_path =
-        "https://image.tmdb.org/t/p/w500" + element["poster_path"];
-      let vote_average = element["vote_average"];
-      let movieId = element["id"];
+const searchInput = document.querySelector("#search-input");
+searchInput.focus();
 
-      let temp_html = `<div class="movie-card" id="movie-card" onclick = "alert('movie id [ ${movieId} ]')">
-                <img src="${poster_path}" alt="">
-                <h3>[${title}]</h3>
-                <p>${overview}</p><br>
-                <h5>평점 : ${vote_average}</h5>
-              </div>`;
-
-      document.getElementById("card-list").innerHTML += temp_html;
-      //$('#card-list').append(temp_html);
-
-      // ---------------------------------------------------------------------
-
-      function filterMovies(movieTitle) {
-        let filterArr = movieList.filter((item) =>
-          item.title.toLowerCase().includes(movieTitle)
-        );
-
-        document.getElementById("card-list").innerHTML = "";
-
-        filterArr.forEach((data) => {
-          let title = data.title;
-          let vote_average = data.vote_average;
-          let overview = data.overview;
-          let poster_path =
-            "https://image.tmdb.org/t/p/w500" + data["poster_path"];
-          let id = data.id;
-
-          let temp_html = `<div class="movie-card" id="movie-card" data-id="${id}" onclick = "alert('movie id [ ${movieId} ]')">
-                <img src="${poster_path}" alt="">
-                <h3>[${title}]</h3>
-                <p>${overview}</p><br>
-                <h5>평점 : ${vote_average}</h5>
-              </div>`;
-
-          document.getElementById("card-list").innerHTML += temp_html;
-        });
-      }
-
-      const searchInput = document.getElementById("search-input");
-      const searchButton = document.getElementById("search-btn");
-
-      function btnClickDisplay() {
-        let movieTitle = searchInput.value.toLowerCase().trim();
-        if (movieTitle === "") {
-          window.location.reload();
-          //alert("제목을 입력하세요!");
-          //return false;
-        } else {
-          filterMovies(movieTitle);
-        }
-      }
-      searchButton.addEventListener("click", btnClickDisplay);
-
-      // ---------------------------------------------------------------------
-    });
-  })
-  .catch((err) => console.error(err));
+const form = document.querySelector("#search-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  filterArr(searchInput.value);
+});
