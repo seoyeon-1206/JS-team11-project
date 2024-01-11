be250d5bd53c69a0f315aa0e2270f6e25196dc32
HaeRheeNam <4ran0318@gmail.com>
2024-01-11 14:35:07 +0900

    [Feat/#7] review 조회 기능-getItem사용할 시, 필요한 주석 추가

--- src/reviewRead.js
+++ src/reviewRead.js
@@ -3,6 +3,7 @@ export const $section = document.querySelector("#reviewRead");
 export const $movieId =
   document.querySelector(".movie-id").attributes["movieid"].value;
 export const $readId = document.querySelectorAll("#reviewRead");
+//임시 데이터
 const reviews = {
   data: [
     {
@@ -22,13 +23,11 @@ const reviews = {
   ],
 };
 
-//localstorage.setItem("review" : )
-console.log({ review: reviews.data });
-
+//localStorage사용 시, 할당되는 값 const datas = JSON.parse(localStorage.getItme("데이터들은 key이름"))
 const datas = [...reviews.data];
 console.log(datas);
-
-reviews.data.map((arr) => {
+//localStorage이용할 시, reviews.data 대신 datas 사용예정.
+datas.map((arr) => {
   const { reviewid, name, review, password, movieid } = arr;
   return $section.insertAdjacentHTML(
     "beforeend",
