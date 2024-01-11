056b900f3661e1e1ecb81c0d53b74e45a35ed640
박영욱 <yeong-uk@MacBook-M1-Pro.local>
2024-01-11 20:36:40 +0900

    [Feat/#18] detail.html id 값 추가 및 reviewReg.js 기능 구현

---
+++ src/reviewReg.js
@@ -0,0 +1,25 @@
+const reviewText = document.querySelector('#review-text');
+const inputName = document.querySelector('#input-name');
+const inputPw = document.querySelector('#input-pw');
+const regBtn = document.querySelector('#reg-btn');
+const movieId = document.querySelector('.movie-id').attributes['movieid'].value;
+
+// 등록 버튼 클릭 시 데이터 객체로 저장 및 배열에 추가
+const dataArr = [];
+const getReviewDatas = function (textVal, nameVal, pwVal) {
+  const reviewData = {
+    textVal,
+    nameVal,
+    pwVal,
+  };
+  
+  dataArr.push(reviewData);
+  const jsonDataArr = JSON.stringify(dataArr)
+  localStorage.setItem('data', jsonDataArr);
+}
+
+// 등록 버튼 클릭 이벤트
+regBtn.addEventListener('click', e => {
+  e.preventDefault();
+  getReviewDatas(reviewText.value, inputName.value, inputPw);
+})
