d4193755e7f37a9d2d1ccbc9ef3e6b5686fec968
박영욱 <yeong-uk@MacBook-M1-Pro.local>
2024-01-12 17:05:24 +0900

    [Feat/#33]reveiwReg.js 코드 수정

--- src/reviewReg.js
+++ src/reviewReg.js
@@ -2,24 +2,32 @@ const reviewText = document.querySelector("#review-text");
 const inputName = document.querySelector("#input-name");
 const inputPw = document.querySelector("#input-pw");
 const regBtn = document.querySelector("#reg-btn");
-// const movieId = document.querySelector('.movie-id').attributes['movieid'].value;
 
+let dataArr = [];
 // 등록 버튼 클릭 시 데이터 객체로 저장 및 배열에 추가
-const dataArr = [];
-const getReviewDatas = function (textVal, nameVal, pwVal) {
+const getReviewDatas = function (review, name, pw) {
+  
+  const currMovieId = new URL(location.href).search.split("?")[1];
+
+  const randomNumber = Math.floor(Math.random() * 10000) + 1;
+
   const reviewData = {
-    textVal,
-    nameVal,
-    pwVal,
+    movieId: currMovieId,
+    userId: randomNumber,
+    name,
+    pw,
+    review,
   };
 
   dataArr.push(reviewData);
-  const jsonDataArr = JSON.stringify(dataArr);
-  localStorage.setItem("data", jsonDataArr);
+  const data = {
+    [currMovieId]: dataArr,
+  };
+  localStorage.setItem('data', JSON.stringify(data));
 };
 
 // 등록 버튼 클릭 이벤트
 regBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  getReviewDatas(reviewText.value, inputName.value, inputPw);
+  getReviewDatas(reviewText.value, inputName.value, inputPw.value);
 });
