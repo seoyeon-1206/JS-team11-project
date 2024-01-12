98a3aef5fd90bb1d9788e4924bcb4da2d2c20d5e
HaeRheeNam <4ran0318@gmail.com>
2024-01-12 12:32:55 +0900

    [Feat/#15] review삭제 기능 테스트 ing

--- src/reviewDel.js
+++ src/reviewDel.js
@@ -1,66 +1,55 @@
-import { currMovieId, $section, datas } from "./reviewRead.js";
-
 //reviewDel.js
+import { currMovieId, $section, reviewReadTemp } from "./reviewRead.js";
+
+export const localDatas = JSON.parse(localStorage.getItem("data"));
+export const datas = [...localDatas];
 const $delBtn = document.querySelector("#review-del-btn");
-
-const reviewReadTemp = (arr) => {
-  arr.map((el) => {
-    const { reviewid, name, review, password, movieid } = el;
-    return $section.insertAdjacentHTML(
-      "beforeend",
-      `
-        <div movidid="${currMovieId}">
-        <div readid="${reviewid}">id: ${reviewid}</div>
-        <p>${review}</p>
-        <p>작성자: ${name}</p>
-        <input type="password" placeholder="password" />
-        <button type="submit" id="review-del-btn">삭제</button>
-        </div>
-        `
-    );
-  });
-};
-// const localData = [...datas];
-console.log(datas); //localStorage 데이터
-
 const reviewDel = (e) => {
-  console.log(datas);
+  console.log(`======로컬 데이터가 들어옵니다`, datas);
   datas.forEach((local) => {
     const { nameVal, textVal, pwVal } = local;
-    //현재 페이지의 써진 댓글 별 id
     const currReadId = e.target.closest("#review-read").querySelector("div>div")
       .attributes.readid.value;
-    //현재 페이지의 삭제 하려는 댓글의 비밀번호
     const currPw = e.target
       .closest("#review-read")
       .querySelector("input").value;
+    const currName = e.target
+      .closest("#review-read")
+      .querySelector("div>p>span").innerText;
+    const currText = e.target
+      .closest("#review-read")
+      .querySelector("div>p").innerText;
+    const viewAfterDel = datas.filter((local) => {
+      console.log(local);
+      return local.nameVal !== currName; // 고유 id 들어오면 고유 id
+    });
 
-    const currName1 = document.querySelector("div");
-    const currName = e.target.closest("#review-read").querySelector("div>p");
-    console.log(currName);
-    const currText = e.target.closest("#review-read").querySelector("div");
-    console.log(currText);
-
-    console.log({ "현재 글의 작성자": 아직 });
-    console.log({ "현재 글의 작성글": 아직 });
-    console.log({ "현재 글의 비밀번호": currPw });
-    console.log({ "현재 글의 비밀번호": currPw });
-    // reivew(storageData)스토리지 데이터의 document상의 movieid, id, password와
-    if (
-      local.movieid === currMovieId &&
-      local.readid === currReadId &&
-      nameVal === currName
-      // && pwVal === currPw
-    ) {
-      localStorage.removeItem(nameVal);
-      localStorage.removeItem(textVal);
-      localStorage.removeItem(pwVal);
+    reviewReadTemp(viewAfterDel);
+    if (nameVal === currName) {
+      //패스워드 들어오면 패스워드
+      console.log(nameVal, `=${currName}`);
+      console.log(`스토리지 정보가 삭제됩니다.`);
+      localStorage.removeItem("nameVal");
+      localStorage.removeItem("textVal");
+      localStorage.removeItem("pwVal");
+      console.log(1);
       // localStorage.removeItem("");
     }
+
+    console.log({ "현재 글의 movieId": currMovieId });
+    console.log({ "현재 글의 작성자 id": currReadId });
+    console.log({ "현재 글의 작성자": currName });
+    console.log({ "현재 글의 작성글": currText });
+    console.log({ "현재 글의 비밀번호": currPw });
+    console.log({ "현재 글의 비밀번호": currMovieId });
   });
 };
 
 $delBtn.addEventListener("click", reviewDel);
+
+// local.movieid === currMovieId &&
+// local.readid === currReadId &&
+// pwVal === currPw
 
 //   if (passWord) {
 //     const stayData = datas.filter(
--- src/reviewRead.js
+++ src/reviewRead.js
@@ -1,30 +1,39 @@
 //reviewRead.js - 해당 영화의 상세페이지의 댓글 내용을
-export const $section = document.querySelector("#review-read");
-const movieId = 123; // 이건 페이지가 연결되면 안
+const movieId = 123; // 이건 페이지가 연결되면 안 써도 괜춘
 const url = new URL(
   `https://api.themoviedb.org/3/movie/${movieId}?language=ko-KR` //location.href;
 );
 export const currMovieId = url.pathname.split("/3/movie/")[1]; //123
+export const $section = document.querySelector("#review-read");
 export const $readId = document.querySelectorAll("#review-read");
 export const localDatas = JSON.parse(localStorage.getItem("data"));
-
-//localStorage사용 시, 할당되는 값 const datas = JSON.parse(localStorage.getItme("데이터들은 key이름"))
 export const datas = [...localDatas];
 console.log(datas);
-//localStorage이용할 시, reviews.data 대신 datas 사용예정.
-datas.map((arr) => {
-  const { nameVal, textVal, pwVal, reviewid, name, review, password, movieid } =
-    arr;
-  return $section.insertAdjacentHTML(
-    "beforeend",
-    `
+export const reviewReadTemp = (arr) => {
+  arr.map((el) => {
+    const {
+      nameVal,
+      textVal,
+      pwVal,
+      reviewid,
+      name,
+      review,
+      password,
+      movieid,
+    } = el;
+    return $section.insertAdjacentHTML(
+      "beforeend",
+      `
            <div movieid="${currMovieId}">
                <div readid="${reviewid}">id: ${reviewid}</div>
                <p>${textVal}</p>
-               <p class="userName">작성자: <span>${nameVal}</span></p>
-               <input type="password" placeholder="password" />
+               <p class="userName">작성자: <span class="name">${nameVal}</span></p>
+               <input type="password" maxlength="4" placeholder="password" />
                <button type="submit" id="review-del-btn">삭제</button>
             </div>
         `
-  );
-});
+    );
+  });
+};
+
+reviewReadTemp(datas);
