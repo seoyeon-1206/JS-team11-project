17a3dadfd795ea9feffa02fdab7314df84187bf8
HaeRheeNam <4ran0318@gmail.com>
2024-01-14 21:41:46 +0900

    [Feat/#49] css 복구완료

--- src/common/reviewReadTemp.js
+++ src/common/reviewReadTemp.js
@@ -6,12 +6,9 @@ import { reviewDel } from "../reviewReg.js";
 export const reviewReadTemp = (arr) => {
   arr.map((el) => {
     const { userId, pw, name, review, movieId, datelist, now_date } = el;
-    console.log(datelist);
     /* 연월일, 시간 구분표시 */
     let y_m_d = datelist.slice(0, 3).join("-"); // ex) 2023-12-12
-    console.log(y_m_d);
     let h_m = datelist.slice(3, 5).join(":"); // ex) 13:22
-    console.log(h_m);
 
     const $readDivMovieId = document.createElement("div");
     $readDivMovieId.setAttribute("movieid", `${movieId}`);
@@ -21,8 +18,15 @@ export const reviewReadTemp = (arr) => {
     $readDivReadId.setAttribute("class", "read-id");
     $readDivReadId.innerText = `id: ${userId}`;
 
-    const $readPreview = document.createElement("p");
-    $readPreview.innerText = `${review}`;
+    const $readReadDetail = document.createElement("p");
+    $readReadDetail.setAttribute("class", "read-detail");
+    $readReadDetail.innerText = `${review}`;
+
+    const $readDivReadList = document.createElement("div");
+    $readDivReadList.setAttribute("class", "read-list");
+
+    const $readDivReadName = document.createElement("div");
+    $readDivReadName.setAttribute("class", "read-name");
 
     const $readPuserName = document.createElement("p");
     $readPuserName.setAttribute("class", "user-name");
@@ -30,15 +34,24 @@ export const reviewReadTemp = (arr) => {
     $readPuserName.innerHTML = `<span class="name">${name}</span>`;
 
     //날짜 - 선택사항
-    const $readDivDate = document.createElement("div");
+    const $readDivDate = document.createElement("p");
     $readDivDate.setAttribute("class", "date");
-    $readDivDate.innerText = `${y_m_d + " " + h_m}`;
+    $readDivDate.innerHTML = `<span>${y_m_d + " " + h_m}</span>`;
+
+    const $readDivReadRight = document.createElement("div");
+    $readDivReadRight.setAttribute("class", "read-right");
+
+    const $readDivReadPw = document.createElement("div");
+    $readDivReadPw.setAttribute("class", "read-pw");
+
+    const $readSpanPw = document.createElement("span");
+    $readSpanPw.innerText = "비밀번호";
 
     const $readInput = document.createElement("input");
     $readInput.setAttribute("type", "password");
     $readInput.setAttribute("maxLength", "4");
     $readInput.setAttribute("placeholder", "password");
-    $readInput.setAttribute("class", "pass-word");
+    $readInput.setAttribute("class", "pass-word input-del-pw");
 
     const $readButton = document.createElement("button");
     $readButton.setAttribute("type", "submit");
@@ -46,13 +59,23 @@ export const reviewReadTemp = (arr) => {
     $readButton.setAttribute("id", `${userId}`);
     $readButton.innerText = `삭제`;
     $readButton.addEventListener("click", (e) => reviewDel($readDivMovieId, e));
-
+    // 구성하는 라인
     $readDivMovieId.append($readDivReadId);
-    $readDivMovieId.append($readPreview);
-    $readDivMovieId.append($readPuserName);
-    $readDivMovieId.append($readDivDate);
-    $readDivMovieId.append($readInput);
-    $readDivMovieId.append($readButton);
+
+    $readDivReadId.append($readReadDetail);
+    $readDivReadId.append($readDivReadList);
+
+    $readDivReadList.append($readDivReadName);
+    $readDivReadList.append($readDivReadRight);
+
+    $readDivReadName.append($readPuserName);
+    $readDivReadName.append($readDivDate);
+
+    $readDivReadRight.append($readDivReadPw);
+    $readDivReadRight.append($readButton);
+
+    $readDivReadPw.append($readSpanPw);
+    $readDivReadPw.append($readInput);
 
     return $section.insertAdjacentElement("afterbegin", $readDivMovieId);
   });
--- src/reviewReg.js
+++ src/reviewReg.js
@@ -72,8 +72,8 @@ regBtn.addEventListener("click", (e) => {
 export const reviewDel = async ($readDivMovieId, e) => {
   e.preventDefault();
   const datas = await JSON.parse(localStorage.getItem(currMovieId));
-  const currPw = e.target.parentNode.children[4].value;
-  const currPwFocus = e.target.parentNode.children[4];
+  const currPw = await e.target.parentNode.children[0].children[1].value;
+  const currPwFocus = e.target.parentNode.children[0].children[1];
   const currUserId = e.currentTarget.id;
 
   //로컬 스토리지에서 삭제하려는 애와 다른 id를 가진 애들 추출한 값
